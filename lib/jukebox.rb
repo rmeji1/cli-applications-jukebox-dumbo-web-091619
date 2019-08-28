866cfdecc213052ca54ec0645cfdbe214d036911
Roberto Mejia <rmeji1@unh.newhaven.edu>
2019-08-28 21:46:49 +0000

    Automatically backed up by Learn

--- lib/jukebox.rb
+++ lib/jukebox.rb
@@ -14,5 +14,5 @@ songs = [
 def help
   puts "I accept the following commands:"
   puts "help : displays this help message"
-  puts "list : displays a list of songs you can pla"
+  puts "list : displays a list of songs you can play"
 end
